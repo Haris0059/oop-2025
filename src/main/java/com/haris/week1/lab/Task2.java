472f927df303859149cd656720f51cfea4d8a40b
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-03-11 17:44:14 +0100

    Week 1, Lab Tasks

--- src/main/java/com/haris/week1/lab/Task2.java
+++ src/main/java/com/haris/week1/lab/Task2.java
@@ -2,10 +2,8 @@ package com.haris.week1.lab;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println("""
-                  /\\
-                 /  \\
-                /____\\  
-                """);
+        System.out.println("  /\\\n" +
+                           " /  \\\n" +
+                           "/____\\");
     }
 }
