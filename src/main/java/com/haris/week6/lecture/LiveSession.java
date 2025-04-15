50c3da78749f70e75707d31208af14c90223f775
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-04-15 19:47:49 +0200

    Week 6, Lab Tasks

---
+++ src/main/java/com/haris/week6/lecture/LiveSession.java
@@ -0,0 +1,19 @@
+package com.haris.week6.lecture;
+
+class Person {
+    private String name;
+    private int age;
+    private int weight;
+    private int height;
+
+    public Person(String name, int weight) {
+        this.name = name;
+        this.age = 0;
+        this.weight = weight;
+        this.height = 0;
+    }
+
+}
+
+class LiveSession {
+}
