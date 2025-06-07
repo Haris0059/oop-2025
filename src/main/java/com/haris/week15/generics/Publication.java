e1b29977c510dcb3df7901d7884d890322ff366b
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-06-07 14:02:48 +0200

    Week 15, Exam Prep

--- src/main/java/com/haris/week15/generics/Publication.java
+++ src/main/java/com/haris/week15/generics/Publication.java
@@ -3,4 +3,20 @@ package com.haris.week15.generics;
 public class Publication {
     private int isbn;
     private String title;
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
